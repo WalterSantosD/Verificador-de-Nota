46c387c59a17132dc313147772c00244a8271de9
WalterSantosD <walter.santoskd@gmail.com>
2022-06-01 16:27:55 -0300

    de

---
+++ src/InterfaceGrafica.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class InterfaceGrafica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, Object> lista = new HashMap<>();
+
+        lista.put(0, new matches());
+
+        lista.get(sc.nextInt());
+        
+        
+
+    }
+}
